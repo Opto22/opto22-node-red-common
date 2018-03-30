8424a0a039695ea24ce287618d2ad8f80dec08f4
Jim Turner <jturner@opto22.com>
2018-03-30 10:04:21 -0700

    EPIC support for getCertFile()

--- src/CertificateUtil.ts
+++ src/CertificateUtil.ts
@@ -15,7 +15,8 @@
 */
 import * as fs from 'fs';
 import * as path from 'path';
-import * as NodeRed  from '../typings/nodered';
+import * as NodeRed from '../typings/nodered';
+import { SystemType } from './SystemType';
 
 export function getCertFile(globalRED: NodeRed.RED, certPath: string): Buffer
 {
@@ -24,12 +25,20 @@ export function getCertFile(globalRED: NodeRed.RED, certPath: string): Buffer
         // See if we have an absolute or relative path
         if (!path.isAbsolute(certPath))
         {
-            // For relative paths, start from Node-RED's userDir + "/certs".
-            certPath = path.join(globalRED.settings.userDir, 'certs', certPath);
+            if (SystemType.isSystemGroovEpic())
+            {
+                // On EPIC, that path is fixed.
+                certPath = path.join('/usr/lib/ssl/certs/', certPath);
+            }
+            else
+            {
+                // On AR1, start from Node-RED's userDir + "/certs".
+                certPath = path.join(globalRED.settings.userDir, 'certs', certPath);
+            }
         }
 
         return fs.readFileSync(certPath);
     }
-    
+
     return null;
 }
