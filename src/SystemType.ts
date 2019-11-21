749d33f6c867e242dfa37101289ece8dffded5a3
Jim Turner <jturner@opto22.com>
2019-11-21 08:47:02 -0800

    Ignore most of SystemType in the test coverage report

--- src/SystemType.ts
+++ src/SystemType.ts
@@ -3,24 +3,27 @@ import fs = require('fs');
 let isGroovBox: boolean;
 let isGroovEpic: boolean;
 
+
 export class SystemType
 {
     static isSystemGroovBox()
     {
+        /* istanbul ignore next */
         if (isGroovBox === undefined) {
             // Look for some obvious marks of a groov Box. This is probably overkill.
             var hasMmpServer = fs.existsSync("/etc/init.d/mmpserver");
             var hasSupervisor = fs.existsSync("/usr/sbin/supervisor-get-serial-number");
             var hasOptoapps = fs.existsSync("/var/lib/jetty/optoapps");
-
+            
             isGroovBox = hasMmpServer && hasSupervisor && hasOptoapps;
         }
-
+        
         return isGroovBox;
     }
-
+    
     static isSystemGroovEpic()
     {
+        /* istanbul ignore next */
         if (isGroovEpic === undefined) {
             // Look for some obvious marks of an EPIC box.
             var hasOptoApps = fs.existsSync("/usr/share/nxtio/");
